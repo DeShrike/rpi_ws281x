ea25ca595b10f47d872083f7df0c2e7f6e350b7f
deshrike <deshrike@hotmail.com>
2024-09-21 17:16:58 +0100

    Add Pi 3 Model A

--- rpihw.c
+++ rpihw.c
@@ -301,6 +301,15 @@ static const rpi_hw_t rpi_hw_info[] = {
         .videocore_base = VIDEOCORE_BASE_RPI2,
         .desc = "Pi 2",
     },
+    // Pi 3 Model A
+    //
+    {
+        .hwver  = 0x9020e0,
+        .type = RPI_HWVER_TYPE_PI2,
+        .periph_base = PERIPH_BASE_RPI2,
+        .videocore_base = VIDEOCORE_BASE_RPI2,
+        .desc = "Pi 3 A+",
+    },
     //
     // Pi 3 Model B
     //
